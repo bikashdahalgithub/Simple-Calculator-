744465ce33c38f20b2310adcd424d81155495cb0
bikashdahalgithub <bikashdahal991@gmail.com>
2022-09-08 07:07:01 +0545

    Taking Input

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+int number1, number2;
+Console.WriteLine("Enter  First Number:");
+number1 = int.Parse (Console.ReadLine());
+Console.WriteLine("Enter Second Number:");
+number2 = int.Parse (Console.ReadLine());
